58343e51625abe85b31b18ab90f564a2c07421a9
xie <283994917@qq.com>
2019-02-26 19:30:51 +0800

    中文注释

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,12 +8,24 @@
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
+
+/*
+ * 这是 Jasmine 会读取的spec文件，它包含所有的要在你应用上面运行的测试。
+ */
+
+/* 我们把所有的测试都放在了 $() 函数里面。因为有些测试需要 DOM 元素。
+ * 我们得保证在 DOM 准备好之前他们不会被运行。
+ */
 $(function() {
+    /* 这是我们第一个测试用例 - 其中包含了一定数量的测试。这个用例的测试
+    * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
+    */
+
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
-    describe('RSS Feeds', function() {
+    describe('订阅阅读器测试', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,6 +33,12 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
+
+        /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
+         * 不是空的。在你开始做这个项目剩下的工作之前最好实验一下这个测试
+         * 比如你把 app.js 里面的 allFeeds 变量变成一个空的数组然后刷新
+         * 页面看看会发生什么。
+        */
         function  notEmpty(data) {
             expect(data).toBeDefined();
             expect(data.length).not.toBe(0);
@@ -28,7 +46,7 @@ $(function() {
         }
 
 
-        it('are defined', function() {
+        it('allFeeds变量被定义了而且不是空的', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -38,7 +56,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('URL defined', function () {
+
+         /* TODO:
+         * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
+         */
+
+        it('有链接字段，并且不是空的', function () {
             for (let feed of allFeeds) {
                 notEmpty(feed.url);
             }
@@ -48,7 +71,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-        it('name defined', function () {
+
+         /* TODO:
+         * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
+         */
+
+        it('有名字，并且不是空的', function () {
             for (let feed of allFeeds) {
                 notEmpty(feed.name);
             }
@@ -57,7 +85,7 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
 
-    describe('The menu',function () {
+    describe('表单',function () {
         let body = document.querySelector("body");
         let menu = document.querySelector('.menu-icon-link');
 
@@ -67,7 +95,12 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
-        it('Menu is hidden ?', function () {
+         /* TODO:
+         * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
+         * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
+         */
+
+        it('表单是否为隐藏的', function () {
             expect(body.className.includes('menu-hidden')).toBe(true);
         });
 
@@ -77,7 +110,13 @@ $(function() {
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
-        it('Menu toggles on or off', function () {
+
+         /* TODO:
+         * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
+         * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
+         * 再次点击的时候是否隐藏。
+         */
+        it('测试表单是否可以正常显示和隐藏', function () {
             menu.click();
             expect(body.className.includes('menu-hidden')).toBe(false);
             menu.click();
@@ -86,8 +125,9 @@ $(function() {
     })
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
 
-    describe("Initial Entries",function () {
+    describe("初始条目",function () {
         let feed = document.querySelector('.feed');
 
         beforeEach(function (done) {
@@ -100,7 +140,17 @@ $(function() {
         * Remember, loadFeed() is asynchronous so this test will require
         * the use of Jasmine's beforeEach and asynchronous done() function.
         */
-        it('a single .entry element within the .feed container. ', function () {
+
+
+         /* TODO:
+         * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
+         * 里面至少有一个 .entry 的元素。
+         *
+         * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
+         * 和异步的 done() 函数。
+         */
+
+        it('.feed容器元素里面至少有一个 .entry的元素 ', function () {
             let entry = feed.querySelectorAll('.entry');
             expect(entry.length).toBeGreaterThan(0);
         });
@@ -110,7 +160,7 @@ $(function() {
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
-    describe('New Feed Selection', function () {
+    describe('条目选择测试', function () {
         let file1,file2;
 
         beforeEach(function (done) {
@@ -128,7 +178,7 @@ $(function() {
         * Remember, loadFeed() is asynchronous.
         */
 
-        it('content actually changes', function (done) {
+        it('条目是否正常切换', function (done) {
             expect(file1).not.toBe(file2);
             done();
         });
